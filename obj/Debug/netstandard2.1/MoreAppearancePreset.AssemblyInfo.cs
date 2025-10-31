9faba01a8680a2d21fb5e48df6d6017887256706
Li Kehang <2353146641@qq.com>
2025-10-31 16:00:49 +0800

    ui: rename some preset

--- obj/Debug/netstandard2.1/MoreAppearancePreset.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/MoreAppearancePreset.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoreAppearancePreset")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7030961d5aab1ac33a60a152bbfbe1884aedc897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258e75681971ed0dbc2238840d32a90ec99578da")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoreAppearancePreset")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoreAppearancePreset")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
